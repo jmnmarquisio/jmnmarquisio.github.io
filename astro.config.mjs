c3b85aeb5d41e87acd2fd662e09176cab3a87bf8
Juan Mateo Nuñez Marquisio <juanmateonunezmarquisio@MacBook-Air-de-Juan.local>
2026-04-08 08:22:46 +0200

    Configure Astro for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,29 +3,11 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import preact from '@astrojs/preact';
-import vercel from '@astrojs/vercel';
-
-// https://astro.build/config
-const site = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : 'https://jvinas.vercel.app';
 
 export default defineConfig({
-  site,
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
-  build: {
-    sourcemap: true,
-  },
+  site: 'https://jmnmarquisio.github.io',
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [sitemap(), preact()]
 });
